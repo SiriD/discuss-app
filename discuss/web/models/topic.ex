fbb7f5b1287e81d79331328b9dccebbbe059642a
SiriD <sirisha.66@gmail.com>
2018-06-14 14:35:03 -0500

    initial commit

---
+++ discuss/web/models/topic.ex
@@ -0,0 +1,14 @@
+defmodule DiscussWeb.Topic do
+  use Discuss.Web, :model
+
+  schema "topics" do
+    field :title, :string
+  end
+
+  def changeset(struct, params \\%{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+end
+
